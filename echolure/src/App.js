6b4143cd15bf0cba8977b76f56ff3bad488d2d1d
Bonnie Remeika <blineweb@gmail.com>
2025-07-17 22:56:00 -0400

    initial commi: Echolure Symbol System

---
+++ echolure/src/App.js
@@ -0,0 +1,13 @@
+import logo from './EcoLore-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🎧 EchoLure</h1>
+      <p>A new visual-musical language begins here.</p>
+    </div>
+  );
+}
+
+export default App;
